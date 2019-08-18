c205de27d5f190aad37302bf0deab94bd98152d1
David Lu <davidjhlu@gmail.com>
2019-08-18 01:11:47 -0400

    Game Files

---
+++ Number Wiz UI/Number Wizard UI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour{
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] Text guessText;
+    int guess;
+
+    // Start is called before the first frame update
+    void Start(){
+        Guesser();
+    }
+
+    public void OnPressHigher() {
+        if(guess != max) {
+            min = guess + 1;
+        }
+        Guesser();
+    }
+
+    public void OnPressLower() {
+        if(guess != min) {
+            max = guess - 1;
+        }
+        Guesser();
+    }   
+
+    void Guesser() {
+        guess = Random.Range(min, max+1);
+        guessText.text = guess.ToString();
+    }
+}
---
+++ Number Wiz UI/Number Wizard UI/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour{
+    
+    public void LoadNextScreen() {
+        int cur = SceneManager.GetActiveScene().buildIndex;
+        if(cur == 2) {
+            SceneManager.LoadScene(0);
+        }
+        else {
+            SceneManager.LoadScene(cur + 1);
+        }
+    }
+
+    public void QuitGame() => Application.Quit();
+}
